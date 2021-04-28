d3731ecba7c00e4a18ef2d94e6a90e9289f2dbc7
Leonardo Arellano Martinez <leo.aremtz98@gmail.com>
2021-04-28 16:20:11 -0500

    Se añade el código para conectarse a la base de datos e insertar información en ella; Se añade un archivo Dockerfile para compilar el API y hacer pruebas en local.

--- backend/gocms/cmd/api/main.go
+++ backend/gocms/cmd/api/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"log"
+
+	"github.com/leosykes117/golb/backend/gocms/pkg/models/user"
+	"github.com/leosykes117/golb/backend/gocms/pkg/storage"
 )
 
 func main() {
-	dbHost := os.Getenv("GOAPI_DB_HOST")
-	dbUser := os.Getenv("GOAPI_DB_USER")
-	dbPass := os.Getenv("GOAPI_DB_PASSWORD")
-	dbName := os.Getenv("GOAPI_DB_NAME")
-	fmt.Println(dbHost)
-	fmt.Println(dbName)
-	fmt.Println(dbPass)
-	fmt.Println(dbUser)
+	dbDriver := storage.MariaDB
+	storage.New(dbDriver)
+	repo, err := storage.DAOUser(dbDriver)
+	if err != nil {
+		log.Fatalf("DAOUser: %v", err)
+	}
+
+	m := user.New("leo.aremtz98@gmail.com", "maleva123", "Leonardo Antonio", "Arellano Martínez", "M", "5514670431")
+
+	if err := repo.Create(m); err != nil {
+		log.Fatalf("ERROR MariaDB on user.Create: %v", err)
+	}
 }
---
+++ backend/gocms/pkg/models/user/user.go
@@ -0,0 +1,35 @@
+package user
+
+// Model es la estructura que representa un usuario.
+type Model struct {
+	ID           uint   `json:"ID"`
+	Email        string `json:"email,omitempty"`
+	PasswordHash string `json:"password,omitempty"`
+	Name         string `json:"name,omitempty"`
+	Surname      string `json:"surname,omitempty"`
+	Gender       string `json:"gender,omitempty"`
+	Phone        string `json:"phone,omitempty"`
+}
+
+// Models es un array de punteros de model
+type Models []*Model
+
+// New retorna un puntero a una nueva instancia de Model
+func New(email, password, name, surname, gender, phone string) *Model {
+	return &Model{
+		Email:        email,
+		PasswordHash: password,
+		Name:         name,
+		Surname:      surname,
+		Gender:       gender,
+		Phone:        phone,
+	}
+}
+
+// Repository es la interfaz que implementa los metodos para la base de datos
+type Repository interface {
+	Create(*Model) error
+	//Update(*Model) error
+	//GetByID(uint) (*Model, error)
+	//Delete(uint) error
+}
